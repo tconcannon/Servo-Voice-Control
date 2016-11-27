59ccbc0129eb139e964ff8f8a1a60dd9dea43ac7
tconcannon <taycon@seas.upenn.edu>
2016-11-26 20:24:01 -0500

    first commit: working voice control with hard-coded servo movement

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+// sockets for voice commands
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//package for arduino control
+var five = require("johnny-five");
+var Board = require("firmata");
+// var board = new Board("path to serialport");
+
+var board = new five.Board();
+board.on("ready", function() {
+  led = new five.Led(13);
+  led.strobe(1000); // on off every second
+  servo = new five.Servo(9); //initialize pwm
+});
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('open', function (data) {
+    console.log(data.command);
+    servo.to(160);
+  });
+  socket.on('close',function(data){
+    console.log(data.command);
+    servo.to(10);
+  })
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
